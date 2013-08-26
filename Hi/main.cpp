cb288e315fc9d201b8493d87d5cce9b19d3e8bcb
Alexander Chen <alexwchenpro@gmail.com>
2013-08-26 00:37:33 -0400

    Initial Commit

---
+++ Hi/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hi
+//
+//  Created by Alexander Chen on 2013-08-26.
+//  Copyright (c) 2013 Alexander Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
